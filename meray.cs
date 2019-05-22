25cee9381c68d6e92b80aa1e9e1a86f536ae6947
mwafik62 <50801599+mwafik62@users.noreply.github.com>
2019-05-22 16:24:19 +0200

    Add files via upload

---
+++ meray.cs
@@ -0,0 +1,28 @@
+using UnitTestTargetProject;
+using Xunit;
+
+namespace MyUnitTests
+{
+    public class MyTargetClassTests
+    {
+        [Fact]
+        public void VerifySummation()
+        {
+            const int intFirstNumber = 15;
+            const int intsecondNumber = 10;
+            var objTargetClass = new MyTargetClass();
+            var intResult = objTargetClass.AddNumbers(intFirstNumber, intsecondNumber);
+            Assert.Equal(15 + 10, intResult);
+        }
+
+        [Fact]
+        public void VerifySubtraction()
+        {
+            const int intFirstNumber = 15;
+            const int intsecondNumber = 10;
+            var objTargetClass = new MyTargetClass();
+            var intResult = objTargetClass.SubractNumbers(intFirstNumber, intsecondNumber);
+            Assert.Equal(15 - 10, intResult);
+        }
+    }
+}
